b8843a425ae4d9d452f612c8bd93801340213e20
Dave Shawley <daveshawley@gmail.com>
2016-02-28 08:59:57 -0500

    Export HTTPClient and HTTPError in the top-level module.

--- sprockets/clients/http/__init__.py
+++ sprockets/clients/http/__init__.py
@@ -1,10 +1,18 @@
 try:
+    from sprockets.clients.http.client import HTTPClient, HTTPError
     from sprockets.clients.http.mixins import ClientMixin
 
 except ImportError as error:
     def ClientMixin(*args, **kwargs):
         raise error
 
+    def HTTPClient(*args, **kwargs):
+        raise error
+
+    def HTTPError(*args, **kwargs):
+        raise error
+
 version_info = (0, 0, 0)
 __version__ = '.'.join(str(v) for v in version_info)
-__all__ = ['version', '__version__', 'ClientMixin']
+__all__ = ['version_info', '__version__',
+           'ClientMixin', 'HTTPClient', 'HTTPError']
